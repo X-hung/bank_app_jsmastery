d7197e21e874a60529cdfb0f101238ac9ecdd93f
X-hung <x.hung.chn@gmail.com>
2022-08-14 11:31:46 +1000

    1-structure-and-layout
    first, create the layout inside of App.jsx file, with some basic styles, then create each Component, use index.js to export all the components, use 'rafce' shorthand to create the template inside each component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React from 'react';
+import styles from './style';
+// Don't import in thi way, create a new index.js file instead in components folder.
+/*
+import NavBar from './components/NavBar';
+import Billing from './components/Billing';
+import Business from './components/Business';
+*/
+// Using index.js to import Components, make it simplified and more readable.
+import { NavBar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <NavBar />
+      </div>
 
-export default App
+    </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
 
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import NavBar from "./NavBar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+
+export {
+    NavBar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    GetStarted,
+    Button,
+    FeedbackCard,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+};
